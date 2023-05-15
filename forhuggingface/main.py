f90aa48b2613518828d0b183ddd5b7087bca1687
Xiang-M-J <3291493948@qq.com>
2023-05-15 18:14:33 +0800

    fix

--- forhuggingface/main.py
+++ forhuggingface/main.py
@@ -5,36 +5,37 @@ import torch
 import torch.nn as nn
 from torch.cuda.amp import GradScaler
 from tqdm import tqdm
-from transformers import Wav2Vec2PreTrainedModel, Wav2Vec2Model, AutoConfig
+from transformers import HubertPreTrainedModel, HubertModel, AutoConfig, Wav2Vec2FeatureExtractor
 
 from data_module import PretrainDataModule
 from pretrain_utils import train_step, val_step
 from utils import Metric
 
-model_name_or_path = "jonatasgrosman/wav2vec2-large-xlsr-53-chinese-zh-cn"
+model_name_or_path = "TencentGameMate/chinese-hubert-base"
 num_class = 6
-epochs = 10
-pooling_mode = 'mean'
+epochs = 30
 spilt_rate = [0.6, 0.2, 0.2]
 use_amp = True
-batch_size = 16
-lr = 1e-4
+batch_size = 36
+lr = 2e-4
 beta1 = 0.93
 beta2 = 0.98
-
+classifier_dropout = 0.1
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
 config = AutoConfig.from_pretrained(
     pretrained_model_name_or_path=model_name_or_path,
 )
 setattr(config, 'num_class', num_class)
-setattr(config, 'pooling_mode', pooling_mode)
+setattr(config, 'classifier_dropout', classifier_dropout)
 
 
-class Wav2Vec2ClassificationHead(nn.Module):
-    """Head for wav2vec2.0 classification task."""
+class HubertClassificationHead(nn.Module):
+    """Head for hubert classification task."""
+
     def __init__(self, config):
         super().__init__()
         self.dense = nn.Linear(config.hidden_size, config.hidden_size)
-        self.dropout = nn.Dropout(0.1)
+        self.dropout = nn.Dropout(classifier_dropout)
         self.out_proj = nn.Linear(config.hidden_size, config.num_class)
 
     def forward(self, x):
@@ -45,43 +46,36 @@ class Wav2Vec2ClassificationHead(nn.Module):
         return x
 
 
-class Wav2Vec2ForSpeechClassification(Wav2Vec2PreTrainedModel):
+class HubertForSpeechClassification(HubertPreTrainedModel):
     def __init__(self, config):
         super().__init__(config)
-        self.wav2vec2 = Wav2Vec2Model(config)
-        self.classifier = Wav2Vec2ClassificationHead(config)
+        self.hubert = HubertModel(config)
+        self.classifier = HubertClassificationHead(config)
         self.init_weights()
 
     def forward(self, x):
-        outputs = self.wav2vec2(x)
+        outputs = self.hubert(x)
         hidden_states = outputs[0]
         x = torch.mean(hidden_states, dim=1)
         x = self.classifier(x)
         return x
 
 
-def train(dataset, paths: list):
+def train(dataset, model, paths: list):
     train_num = dataset.train_num
     val_num = dataset.val_num
 
     train_loader = dataset.train_dataloader(batch_size=batch_size)
     val_loader = dataset.val_dataloader(batch_size=batch_size)
-
-    model = Wav2Vec2ForSpeechClassification.from_pretrained(
-        model_name_or_path,
-        config=config,
-    )
-    model.push_to_hub("test")
-    model.gradient_checkpointing_enable()
     parameter = []
-    for name, param in model.named_parameters():
-        if "wav2vec" in name:
+    for name, param in model.named_parameters():  # 仅训练classifier.dense和classifier.out_proj
+        if "hubert" in name:
             parameter.append({'params': param, 'lr': 0.2 * lr})
         else:
             parameter.append({'params': param, "lr": lr})
 
     optimizer = torch.optim.AdamW(parameter, lr=lr, betas=(beta1, beta2), weight_decay=0.2)
-    scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=4, gamma=0.3)
+    scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=10, gamma=0.3)
     if use_amp:
         scaler = GradScaler()
     else:
@@ -111,9 +105,9 @@ def train(dataset, paths: list):
         train_loss = train_loss / (math.ceil(train_num / batch_size))
         val_acc = val_correct / val_num
         val_loss = val_loss / (math.ceil(train_num / batch_size))
-        metric.train_acc.append(train_acc)
+        metric.train_acc.append(train_acc * 100)
         metric.train_loss.append(train_loss)
-        metric.val_acc.append(val_acc)
+        metric.val_acc.append(val_acc * 100)
         metric.val_loss.append(val_loss)
         print(
             "epoch: {}, train_accuracy: {:.3f}\t train_loss: {:.4f}; \t val_accuracy: {:.3f}\t val_loss: {:.4f}".format(
@@ -127,8 +121,6 @@ def train(dataset, paths: list):
         scheduler.step()
         np.save(paths[2], metric.item())
         torch.save(model, paths[0])
-        state = {"optimizer": optimizer.state_dict(), "scheduler": scheduler.state_dict(), 'epoch': epoch}
-        torch.save(state, paths[3])
 
 
 def m_test(model_path: str, dataset):
@@ -150,9 +142,17 @@ def m_test(model_path: str, dataset):
 
 
 if __name__ == "__main__":
-    paths = ['models/casia.pt', 'models/casia_best.pt',  "results/metric.npy", "results/optim_casia.pth"]
-    dataset = PretrainDataModule(model_name_or_path, 6, "casia_y.npy", "../preprocess/datasets/CASIA", duration=6)
+    paths = ['models/hubert_ser_casia_final.pt', 'models/hubert_ser_casia_best.pt', "results/hubert_casia.npy",]
+    processor = Wav2Vec2FeatureExtractor.from_pretrained(model_name_or_path)
+    dataset = PretrainDataModule(num_class, "casia_y.npy", "../preprocess/datasets/CASIA", processor, duration=6)
     dataset.setup()
-    train(dataset, paths)
+    model = HubertForSpeechClassification.from_pretrained(
+        model_name_or_path,
+        config=config,
+    )
+    model.gradient_checkpointing_enable()
+    train(dataset, model, paths)
     m_test(paths[0], dataset)
     m_test(paths[1], dataset)
+    processor.push_to_hub("hubert-base-ch-speech-emotion-recognition")
+    model.push_to_hub("hubert-base-ch-speech-emotion-recognition")
